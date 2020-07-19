28ceeb6d76f912d41d35ff20705b0cb1a17a2b51
Masashi Yoshikawa <yoshikawa.masashi.yh8@is.naist.jp>
2020-07-20 03:31:27 +0900

    update

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='spigot',
     version='0.0.0',
-    install_requires=['spigot'],
+    # install_requires=['spigot'],
+    packages=find_packages(),
+    license='MIT License',
     extras_require={},
 )
